fe71c1af9d30b045ead23149e0cd8c6e8d0e5c17
khurramraheel <khurramraheel@yahoo.com>
2025-07-18 17:18:49 +0500

    main

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 let mongoose =  require('mongoose')
 
 
-mongoose.connect('mongodb://localhost:27017/sigmaDB').then((connection)=>{
+mongoose.connect('mongodb+srv://testWala:Abc_123@cluster0.hgfh1gx.mongodb.net/sigmaDB').then((connection)=>{
     console.log(connection);
 })
